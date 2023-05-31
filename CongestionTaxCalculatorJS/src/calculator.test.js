2a05dcdc40b31bbbca469b74d7b53f31389de6e8
Fredrik Lunde <fredrik.lunde3@gmail.com>
2023-05-31 23:52:56 +0200

    refactor

--- CongestionTaxCalculatorJS/src/calculator.test.js
+++ CongestionTaxCalculatorJS/src/calculator.test.js
@@ -1,16 +1,15 @@
 const printTotalAmount = require("./calculator");
 
-describe('printTotalAmount', () => {
+describe("printTotalAmount", () => {
   beforeEach(() => {
-    jest.spyOn(console, 'log').mockImplementation(() => {});
+    jest.spyOn(console, "log").mockImplementation(() => {});
   });
 
   afterEach(() => {
     console.log.mockRestore();
   });
 
-  it('should print the correct congestion charge amount', () => {
-
+  it("should print the correct congestion charge amount", () => {
     const dateTimeString = "2023-05-31 17:45";
     const expectedOutput = "The total fee is 13 kr";
 
@@ -19,9 +18,9 @@ describe('printTotalAmount', () => {
     expect(console.log).toHaveBeenCalledWith(expectedOutput);
   });
 
-  it('should print the correct congestion charge amount when given several passages', () => {
-
-    const dateTimeStringList = "2023-05-31 08:00, 2023-05-31 12:00, 2023-05-31 12:45, 2023-05-31 17:45";
+  it("should print the correct congestion charge amount when given several passages", () => {
+    const dateTimeStringList =
+      "2023-05-31 08:00, 2023-05-31 12:00, 2023-05-31 12:45, 2023-05-31 17:45";
     const expectedOutput = "The total fee is 42 kr";
 
     printTotalAmount(dateTimeStringList);
@@ -29,15 +28,13 @@ describe('printTotalAmount', () => {
     expect(console.log).toHaveBeenCalledWith(expectedOutput);
   });
 
-  it('should never print more than 60 kr per day', () => {
-
-
-    const dateTimeStringList = "2023-05-31 07:00, 2023-05-31 07:10, 2023-05-31 07:20, 2023-05-31 07:30";
+  it("should never print more than 60 kr per day", () => {
+    const dateTimeStringList =
+      "2023-05-31 07:00, 2023-05-31 07:10, 2023-05-31 07:20, 2023-05-31 07:30";
     const expectedOutput = "The total fee is 60 kr";
 
     printTotalAmount(dateTimeStringList);
 
     expect(console.log).toHaveBeenCalledWith(expectedOutput);
   });
-
 });
